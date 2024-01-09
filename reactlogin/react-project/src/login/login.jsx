e663feb1765da6db15f4a66080974b30d90d2f2c
ShobikaVishnu <154575970+ShobikaVishnu@users.noreply.github.com>
2024-01-09 09:34:24 +0530

    Add files via upload

---
+++ reactlogin/react-project/src/login/login.jsx
@@ -0,0 +1,36 @@
+import "../login/login.css"
+// import Register from "../register/register"
+
+export default function login() {
+  return (
+    <>
+    <div className="loginPage">
+      <div className="loginContent">
+                <span className="loginTitle">Login</span>
+                <form action="" className="loginForm">
+                    <label htmlFor="">Email Id</label>
+                    <input type="text"  id="" placeholder="Enter Your Email ID" />
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="" id="" placeholder="Enter Your Password" />
+                    <button className="loginBtn">Login</button>
+                </form>
+            </div>
+    </div>
+      </>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
